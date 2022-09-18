4c290e2608d9df3a895f439274b35176d30c704a
sadaf <112341232+utashell@users.noreply.github.com>
2022-09-18 10:03:46 +0430

    Add files via upload

---
+++ connect.py
@@ -0,0 +1,29 @@
+import sqlite3 as sq
+con = sq.connect('C:/Users/sadaf/Desktop/project/bank.db')
+print('ok')
+c = con.cursor()
+com = """create table user(account_num integer PRIMARY KEY AUTOINCREMENT,
+        id_user integer ,
+        firstname Text(20),
+        lastname Text(30),
+        middle_name Text(20) ,
+        age integer,
+        gender Text(8),
+        city Text(20),
+        country Text(20),
+        phone_number integer(11),
+        email Text(40),
+        address Text(50),
+        username Text (20),
+        password Text(20),
+        date_account Text(50))"""
+try:
+
+    c.execute(com)
+    con.commit()
+except:
+    print('error')
+    con.rollback()
+
+con.close()
+
